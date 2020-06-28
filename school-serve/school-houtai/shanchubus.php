da7ceb7d11686926f480844d5a4f6691e35be48e
ChenMingXingOpen <43124731+ChenMingXingOpen@users.noreply.github.com>
2020-06-28 23:36:37 +0800

    第一次上传

---
+++ school-serve/school-houtai/shanchubus.php
@@ -0,0 +1,20 @@
+// +----------------------------------------------------------------------
+// | 学号：2017051604055
+// +----------------------------------------------------------------------
+// | Author: 陈明欣
+// +----------------------------------------------------------------------
+<?php 
+include "conn/conn.php";
+$a=$_GET['name'];
+$b=$_GET['studentID'];
+$p="delete from tb_bus where name='$a' and studentID='$b'";
+$w=mysqli_query($conn,$p);
+if($w) 
+{
+	echo "<script>alert('删除信息成功');history.go(-1);</script>";
+}
+else
+{
+	echo "<script>alert('删除信息失败');history.go(-1);</script>";
+}
+?>
