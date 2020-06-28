54beffdb84e0e0a4009457cf1313b85840616fa1
Lanala-fall <51905366+Lanala-fall@users.noreply.github.com>
2020-06-28 19:27:28 +0800

    第二次

---
+++ School-serve/addserver.php
@@ -0,0 +1,194 @@
+// +----------------------------------------------------------------------
+// | 学号：2017051604012
+// +----------------------------------------------------------------------
+// | Author: 缪淑婷
+// +----------------------------------------------------------------------
+// | content: 周边服务(后台):优惠活动
+// +----------------------------------------------------------------------
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+    <body>
+    <script>
+    function showserver()
+    {
+    	var a=document.getElementById('lei').value;
+    	if(a=="")
+    	{
+    		alert('请输入所属大类型!');
+    		return false;
+    	}
+    	var b=document.getElementById('leixing').value;
+    	if(b=="")
+    	{
+    		alert('请输入所属小类型');
+    		return false;
+    	}
+    	var c=document.getElementById('time').value;
+    	if(c=="")
+    	{
+    		alert('请输入优惠时间段!');
+    		return false;
+    	}
+    	var e=document.getElementById('where').value;
+    	if(e=="")
+    	{
+    		alert('请输入优惠地点!');
+    		return false;
+    	}
+    	var f=document.getElementById('guiz').value;
+    	if(f=="")
+    	{
+    		alert('请输入优惠规则!');
+    		return false;
+    	}
+    	var g=document.getElementById('url').value;
+    	if(g=="")
+    	{
+    		alert('请输入宣传图片地址!');
+    		return false;
+    	}
+        var elements = document.getElementsByClassName("v");
+
+        var formData =new FormData();
+        for(var i=0; i<elements.length; i++)
+        {
+            
+            formData.append(elements[i].name,elements[i].value);
+            
+        }
+
+        var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function()
+            {
+                if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                {
+                	alert(xmlHttp.responseText);
+                }
+            }
+            xmlHttp.open("post", "saveserve.php"); 
+            xmlHttp.send(formData); 
+    }
+    
+    </script>
+        <table width="100%" border="0" cellpadding="0" cellspacing="0">
+
+            <tr>
+                <td valign="top" bgcolor="#F7F8F9">
+                    <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                        <tr><td colspan="2" valign="top">&nbsp;</td><td>&nbsp;</td><td valign="top">&nbsp;</td></tr>
+                        <tr>
+                            <td colspan="4">
+                                <table>
+                                    <tr>
+                                        <td width="100" align="center"><img src="Images/3.png" /></td>
+                                        <td valign="bottom"><h3 style="letter-spacing:1px;">请填写要添加优惠的详细信息</h3></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td height="40" colspan="4">
+                                <table width="100%" height="1" border="0" cellpadding="0" cellspacing="0" bgcolor="#CCCCCC">
+                                    <tr><td></td></tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <!-- 添加栏目开始 -->
+                        <tr>
+                            <td width="2%">&nbsp;</td>
+                            <td width="96%">
+                                <table width="100%">
+                                    <tr>
+                                        <td colspan="2">
+                                            <form>
+                                                <table width="100%" class="cont">
+                                                    <tr>
+                                                        <td width="2%">&nbsp;</td>
+                                                        <td>优惠所属大类型：</td>
+                                                        <td width="20%"><input class="v" type="text" name="lei" id="lei"></td>
+                                                        <td>设置所属的大类型</td>
+                                                        <td width="2%">&nbsp;</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>&nbsp;</td>
+                                                        <td>优惠所属小类型：</td>
+                                                        <td>
+                                                            <input type="text" class="v" name="leixing" id="leixing">
+                                                        </td>
+                                                        <td>设置老师所属学院</td>
+                                                        <td>&nbsp;</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>&nbsp;</td>
+                                                        <td>优惠的时间：</td>
+                                                        <td>
+                                                            <input type="text" class="v" name="time" id="time">
+                                                        </td>
+                                                        <td>设置优惠的开始时间和结束时间</td>
+                                                        <td>&nbsp;</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>&nbsp;</td>
+                                                        <td>优惠的地点：</td>
+                                                        <td>
+                                                         	<input type="text" class="v" name="where" id="where">
+                                                        </td>
+                                                        <td>设置优惠店铺的地点</td>
+                                                        <td>&nbsp;</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>&nbsp;</td>
+                                                        <td>优惠规则：</td>
+                                                        <td>
+                                                        <input type="text" class="v" name="guiz" id="guiz">
+                                                        </td>
+                                                        <td>设置优惠的规则</td>
+                                                        <td>&nbsp;</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>&nbsp;</td>
+                                                        <td>优惠宣传图片的地址：</td>
+                                                        <td><input class="v" style="width:200px;" type="text" name="url" id="url"/></td>
+                                                        <td>设置宣传图片的相对位置</td>
+                                                        <td>&nbsp;</td>
+                                                    </tr>
+                                                 
+                                                    <tr>
+                                                        <td>&nbsp;</td>
+                                                        <td colspan="3"><input class="btn" type="button" value="提交"  onclick="return showserver()"/></td>
+                                                        <td>&nbsp;</td>
+                                                    </tr>
+                                                </table>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                            <td width="2%">&nbsp;</td>
+                        </tr>
+                        <!-- 添加栏目结束 -->
+                        <tr>
+                            <td height="40" colspan="4">
+                                <table width="100%" height="1" border="0" cellpadding="0" cellspacing="0" bgcolor="#CCCCCC">
+                                    <tr><td></td></tr>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td width="2%">&nbsp;</td>
+                            <td width="51%" class="left_txt">
+                                <img src="Images/email.gif" width="16" height="11"> 客户服务邮箱：1747495714@qq.com<br />
+                                <img src="Images/phone.gif" width="17" height="14">客服服务电话：13364068425
+                            </td>
+                            <td>&nbsp;</td><td>&nbsp;</td>
+                        </tr>
+                    </table>
+                </td>
+                <td background="./Images/mail_right_bg.gif">&nbsp;</td>
+            </tr>
+          
+        </table>
+    </body>
+</html>
---
+++ School-serve/servemanage.php
@@ -0,0 +1,161 @@
+// +----------------------------------------------------------------------
+// | 学号：2017051604012
+// +----------------------------------------------------------------------
+// | Author: 缪淑婷
+// +----------------------------------------------------------------------
+// | content: 周边服务(后台):优惠信息
+// +----------------------------------------------------------------------
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style type="text/css">
+    .w
+    {
+    color:#FFFFFF;
+    }
+    .w:hover
+    {
+    color:orange;
+    }
+    
+    </style>
+</head>
+   <body>
+    <script>
+     function openfindpwd()
+	{
+		window.open("xiugai.php","newframe","left=400,top=200,width=400,height=200,menubar=no,toolbar=no,location=no,scrollbars=no,location=no");
+	}
+ 	</script>
+    <form method="get">
+                                <table height="90%">
+                                    <tr>
+                                        <td width="100" align="center" colspan="2"><img src="Images/3.png" /></td>
+                                        <td valign="bottom" colspan="5" bgcolor="#F7F8F9"><h3 style="letter-spacing:1px;">在这里，您可以根据您的需求，修改网站参数！</h3></td>
+                                    </tr>
+    
+                        <!-- 添加栏目开始 -->
+                        <tr bgcolor="#74adaa" >
+                                                        <th class="w">优惠大类型</th>
+                                                        <th width="70px" class="w">优惠小类型</th>
+                                                        <th width="80px" class="w">优惠时间</th>
+                                                        <th width="80px" class="w">优惠地点</th>
+                                                        <th width="50px" class="w">优惠规则</th>
+                                                        <th width="50px" class="w">优惠宣传图片路径</th>
+                                                        <th width="80px" class="w">操作</th>
+                                                       
+                                                        
+                                                    </tr>
+                                                    
+                                                    <?php 
+													$link=mysqli_connect('localhost','root','root'); 
+													mysqli_select_db($link,'shixun'); 
+													mysqli_query($link,'set names utf8'); 
+
+													$Page_size=3; 
+													$result=mysqli_query($link,"select * from tb_youhui"); 
+													$count = mysqli_num_rows($result); 
+													$page_count = ceil($count/$Page_size); 
+
+													$init=1; 
+													$page_len=7; 
+													$max_p=$page_count; 
+													$pages=$page_count; 
+
+													//判断当前页码 
+													if(empty($_GET['page'])||$_GET['page']<0){ 
+														$page=1; 
+													}else { 
+														$page=$_GET['page']; 
+													} 
+
+													$offset=$Page_size*($page-1); 
+													$sql="select * from tb_youhui limit $offset,$Page_size";
+													$result=mysqli_query($link,$sql); 
+													while ($row=mysqli_fetch_array($result)) { 
+?> 
+<tr> 
+<td bgcolor="#E0EEE0" ><div align="center"> 
+<?php echo $row['main']?> 
+</div></td> 
+<td bgcolor="#E0EEE" ><div align="center"> 
+<?php echo $row['little']?> 
+</div></td> 
+<td bgcolor="#E0EEE"><div align="center"> 
+<?php echo $row['time']?> 
+</div></td> 
+<td bgcolor="#E0EEE"><div align="center"> 
+<?php echo $row['where']?> 
+</div></td> 
+<td bgcolor="#E0EEE"><div align="center"> 
+<?php echo $row['youhui']?> 
+</div></td> 
+<td bgcolor="#E0EEE"><div align="center"> 
+<?php echo $row['lujin']?> 
+</div></td>
+<td bgcolor="#E0EEE"><a href="shanchuserver.php?main=<?php echo $row['main'];?> & little=<?php echo $row['lujin'];?>">删除</a> &nbsp;&nbsp;
+
+</tr> 
+<?php 
+} 
+$page_len = ($page_len%2)?$page_len:$pagelen+1;//页码个数 
+$pageoffset = ($page_len-1)/2;//页码个数左右偏移量 
+
+$key='<div class="page">'; 
+$key.="<span>$page/$pages</span> "; //第几页,共几页 
+if($page!=1){ 
+$key.="<a href='?page=1'>第一页</a> "; //第一页 
+$key.="<a href='?page=".($page-1)."'>上一页</a>"; //上一页 
+}else { 
+$key.="第一页 ";//第一页 
+$key.="上一页"; //上一页 
+} 
+if($pages>$page_len){ 
+//如果当前页小于等于左偏移 
+if($page<=$pageoffset){ 
+$init=1; 
+$max_p = $page_len; 
+}else{//如果当前页大于左偏移 
+//如果当前页码右偏移超出最大分页数 
+if($page+$pageoffset>=$pages+1){ 
+$init = $pages-$page_len+1; 
+}else{ 
+//左右偏移都存在时的计算 
+$init = $page-$pageoffset; 
+$max_p = $page+$pageoffset; 
+} 
+} 
+} 
+for($i=$init;$i<=$max_p;$i++){ 
+if($i==$page){ 
+$key.=' <span>'.$i.'</span>'; 
+} else { 
+$key.=" <a href='?page=".$i."'>".$i."</a>"; 
+} 
+} 
+if($page!=$pages){ 
+$key.=" <a href='?page=".($page+1)."'>下一页</a> ";//下一页 
+$key.="<a href='?page=".$pages."'>最后一页</a>"; //最后一页 
+}else { 
+$key.="下一页 ";//下一页 
+$key.="最后一页"; //最后一页 
+} 
+$key.='</div>'; 
+?> 
+<tr> 
+<td colspan="9" bgcolor="#E0EEE0"><div align="center"><?php echo $key?></div></td> 
+</tr> 
+
+                        <tr>
+                            <td width="2%">&nbsp;</td>
+                            <td colspan="5">
+                                <img src="Images/email.gif" width="16" height="11"> 客户服务邮箱：1747495714@qq.com<br />
+                                <img src="Images/phone.gif" width="17" height="14">客户服务电话：13364068425
+                            </td>
+                            <td>&nbsp;</td><td>&nbsp;</td>
+                        </tr>
+                    
+        </table>
+        </form>
+    </body>
+</html>
